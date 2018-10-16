fe807509dff02f3816186095b52628118e782520
Alexis Breton <alexis95150@gmail.com>
2018-10-16 14:16:43 +0200

    Navbar et background header

---
+++ allovoisin/web/pages/accueil.php
@@ -0,0 +1,19 @@
+<?php include("header.php"); ?>
+
+<header class="container-fluid header-accueil">
+
+    <div class="container">
+
+	<div class="col-12-md col-6">
+	    <h1>Le coup de pouce du quotidien !</h1>
+	    <p> 500 000 voisins échangent au quotidien partout en France.</p>
+	</div>
+
+	<div class="col-12-md col-6">
+	</div>
+	
+    </div>
+
+</header>
+
+<?php include("footer.php"); ?>
---
+++ allovoisin/web/pages/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+
+    
+    <head>
+	
+	<title>HTML Tutorial</title>
+	<meta charset="utf-8" />
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope|Gaegu:400,700|Schoolbell" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	
+    </head>
+
+
+    
+    
+    <body>
+
+	
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    <div class="container">
+		<a class="navbar-brand" href="#">AlloVoisin</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+			    <a class="nav-link" href="#">Accueil</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link" href="#">Chercher</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link" href="#">Proposer</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link" href="#">Connexion</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link" href="#">Inscription</a>
+			</li>
+		    </ul>
+		</div>
+	    </div>
+	</nav>
+	
