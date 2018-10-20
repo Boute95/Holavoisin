fa863c88cd7ad0f98715652a81b510c87bdc9c68
Alexis Breton <alexis95150@gmail.com>
2018-10-20 19:08:44 +0200

    Change couleur de fond des boutons de la navbar

--- allovoisin/web/pages/header.php
+++ allovoisin/web/pages/header.php
@@ -5,7 +5,7 @@
     
     <head>
 	
-	<title>HTML Tutorial</title>
+	<title>¡HolaVoisin!</title>
 	<meta charset="utf-8" />
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -23,7 +23,7 @@
 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	    <div class="container">
-		<a class="navbar-brand" href="#">AlloVoisin</a>
+		<a class="navbar-brand" href="#">¡HolaVoisin!</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		</button>
