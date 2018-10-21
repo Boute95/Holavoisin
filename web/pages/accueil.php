b7bc083df50718dd19a4da9f4c0ff70fa158db62
Alexis Breton <alexis95150@gmail.com>
2018-10-21 11:35:21 +0200

    Utilise flexbox pour le contenu de l'accueil et regle des problemes de responsivite

--- web/pages/accueil.php
+++ web/pages/accueil.php
@@ -4,41 +4,43 @@
 
     <div class="container">
 
-    <div class= "row">
-	<div class="col-6">
-	    <h1>Le coup de pouce du quotidien !</h1>
-	    <p>500 000 voisins échangent au quotidien partout en France.</p>
-	</div>
+	<div class= "row my-auto">
+	    
+	    <div class="col-12 col-lg-6 text-center text-lg-left mb-3 mb-lg-0">
+		<h1>Le coup de pouce du quotidien !</h1>
+		<p>500 000 voisins échangent au quotidien partout en France.</p>
+	    </div>
 
-	<div class="col-6">
+	    <div class="col-12 col-lg-6">
 		<div class="formulaire mx-auto">
-		<form method="post" action="">
-		    <label class="row">Ville</label>
-		    <select class="row mb-2" type="text" id="ville" name="ville" placeholder="">
-		    	<option value="montpellier">Montpellier</option>
-		    	<option value="paris">Paris</option>
-		    	<option value="nice">Nice</option>
-		    </select>
+		    <form method="post" action="">
+			<label class="row">Ville</label>
+			<select class="row mb-2" type="text" id="ville" name="ville" placeholder="">
+		    	    <option value="montpellier">Montpellier</option>
+		    	    <option value="paris">Paris</option>
+		    	    <option value="nice">Nice</option>
+			</select>
 
-		    <label class="row">Catégorie</label>
-		    <select class="row mb-2" type="text" id="catégorie" name="catégorie" placeholder="???">
-		    	<option value="montpellier">Montpellier</option>
-		    	<option value="paris">Paris</option>
-		    	<option value="nice">Nice</option>
-		    </select>
+			<label class="row">Catégorie</label>
+			<select class="row mb-2" type="text" id="catégorie" name="catégorie" placeholder="???">
+		    	    <option value="montpellier">Montpellier</option>
+		    	    <option value="paris">Paris</option>
+		    	    <option value="nice">Nice</option>
+			</select>
 
-		    <div class= "row mt-4">
-		    	<div class="col-6">
+			<div class= "row mt-4">
+		    	    <div class="col-6">
 			    	<input class="mx-auto" type="submit" id="chercher" value="Chercher">
-				</div>
-				<div class="col-6">
-			  		<input class="mx-auto" type="submit" id="proposer" value="Proposer">
-				</div>
+			    </div>
+			    <div class="col-6">
+			  	<input class="mx-auto" type="submit" id="proposer" value="Proposer">
+			    </div>
 			</div>
-		</form>
-	</div>
-	</div>
-	
+		    </form>
+		</div>
+		
+	    </div>
+	    
 	</div>
     </div>
 
