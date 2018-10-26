d86a8bc5bb85ee8f07b86ef97176d1a2db192606
Alexis Breton <alexis95150@gmail.com>
2018-10-26 23:13:54 +0200

    Remet le dossier database supprime par erreur

---
+++ database/insertion.sql
@@ -0,0 +1,3 @@
+insert into services values (1, "Creation de site web", "Je suis développeur web et je propose mes services de création de site web");
+insert into services values (2, "Ménage", "Je propose de faire du ménage");
+insert into services values (3, "Courses", "Je suis étudiant et je suis véhiculé pour faire les courses");
