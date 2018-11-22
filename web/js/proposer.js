55edf02a7b1fe5b012f7c5719524696995b92322
meriem <ameraouim@gmail.com>
2018-11-22 02:12:41 -0800

    Animation proposer

--- web/js/proposer.js
+++ web/js/proposer.js
@@ -1,17 +1,27 @@
 $().ready(function() {
 
+  var selectedType;
+
     $("#continuer1").on("click", function(){
+
+      // Get the selected type
+      if(document.getElementById('type-objet').checked) {
+        selectedType = 'objet';
+      }
+      else {
+        selectedType = 'service';
+      }
 
       $("#form-type").css({
 	  left: '-100px',
 	  transform: 'translate(-100%, -50%)'
       });
 
-      $('#form-objet').css({
+      $('#form-' + selectedType).css({
 	  left: '50%',
 	  transform: 'translate(-50%, -50%)'
-      })
-	
+  });
+
     });
 
     $(".retour").on('click', function(){
@@ -21,10 +31,10 @@ $().ready(function() {
 	    transform: 'translate(-50%, -50%)'
 	});
 
-	$('#form-objet').css({
+	$('#form-' + selectedType).css({
 	    transform: 'translate(110vw, -50%)'
-	})
-	
+	});
+
     });
 
 });
